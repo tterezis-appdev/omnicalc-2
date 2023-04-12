d91584053f068670fb9d572f6b04384ae56a6aad
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-12 04:29:04 +0000

    Addition Form Working

---
+++ app/controllers/math_controller.rb
@@ -0,0 +1,16 @@
+class MathController < ActionController::Base
+
+  
+  def
+    addition_form
+    render ({:template => "addition.html.erb"})
+  end
+
+  # @first=params.fetch("first_num")
+
+  # @second=params.fetch("second_num")
+
+  # @result=@first+@second
+
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+get("/wizard_add",{:controller => "math", :action=> "addition_form"})
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
