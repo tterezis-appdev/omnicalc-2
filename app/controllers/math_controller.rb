06c63a62c0f4852e89266b8dae66a4c9d6b30ee6
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-12 05:16:26 +0000

    Need to commit addition_results

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,16 +1,24 @@
 class MathController < ActionController::Base
-
-  
+#forms
   def
     addition_form
-    render ({:template => "addition.html.erb"})
+    render({:template => "addition.html.erb"})
   end
 
+#results
+  def
+  addition_results
+  render({:template => "addition_results.html.erb"})
   # @first=params.fetch("first_num")
 
   # @second=params.fetch("second_num")
 
   # @result=@first+@second
-
   end
+end
 
+
+
+
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 get("/wizard_add",{:controller => "math", :action=> "addition_form"})
 
+get("/addition_results",{:controller=>"math", :action=> "addition_results"})
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
